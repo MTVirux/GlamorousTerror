98e33da52a7e6a9458b11affd027996b6bcb6a1f
MTVirux <joaofraga96@outlook.com>
2026-05-23 04:04:41 +0100

    Add WildcardIdentifier shim for wildcard actor construction

---
+++ Glamourer/GlamorousTerror/WildcardAutomation/WildcardIdentifier.cs
@@ -0,0 +1,31 @@
+using Dalamud.Game.ClientState.Objects.Enums;
+using Penumbra.GameData.Actors;
+using Penumbra.GameData.Enums;
+using Penumbra.GameData.Structs;
+using Penumbra.String;
+
+namespace Glamourer.GlamorousTerror.WildcardAutomation;
+
+internal static class WildcardIdentifier
+{
+    public static bool IsWildcard(ByteString name)
+        => !name.IsEmpty && name.ToString().Contains('*');
+
+    public static bool IsWildcard(string? name)
+        => !string.IsNullOrEmpty(name) && name.Contains('*');
+
+    public static ActorIdentifier PlayerOrFallback(ActorManager actors, ByteString name, WorldId world)
+        => IsWildcard(name)
+            ? actors.CreateIndividualUnchecked(IdentifierType.Player, name, world.Id, ObjectKind.Pc, 0)
+            : actors.CreatePlayer(name, world);
+
+    public static ActorIdentifier RetainerOrFallback(ActorManager actors, ByteString name, ActorIdentifier.RetainerType type)
+        => IsWildcard(name)
+            ? actors.CreateIndividualUnchecked(IdentifierType.Retainer, name, (ushort)type, ObjectKind.Retainer, 0)
+            : actors.CreateRetainer(name, type);
+
+    public static ActorIdentifier OwnedOrFallback(ActorManager actors, ByteString name, WorldId world, ObjectKind kind, NpcId data)
+        => IsWildcard(name)
+            ? actors.CreateIndividualUnchecked(IdentifierType.Owned, name, world.Id, kind, data)
+            : actors.CreateOwned(name, world, kind, data);
+}
